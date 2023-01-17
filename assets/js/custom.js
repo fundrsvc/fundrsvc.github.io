3b76e92bbf5205602ba67427a17f9e101d57e598
Hashem Zahran <hezahran@gmail.com>
2023-01-17 14:43:49 +0300

    updating soon page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,110 +1,39 @@
-$(window).on('load', function(){
-
-	"use strict";
- 
- 
-	/* ========================================================== */
-	/*   Navigation Background Color                              */
-	/* ========================================================== */
-	
-	$(window).on('scroll', function() {
-		if($(this).scrollTop() > 450) {
-			$('.navbar-fixed-top').addClass('opaque');
-		} else {
-			$('.navbar-fixed-top').removeClass('opaque');
-		}
-	});
- 
-	
-	/* ========================================================== */
-	/*   Hide Responsive Navigation On-Click                      */
-	/* ========================================================== */
-	
-	  $(".navbar-nav li a").on('click', function(event) {
-	    $(".navbar-collapse").collapse('hide');
-	  });
-
-	
-	/* ========================================================== */
-	/*   Navigation Color                                         */
-	/* ========================================================== */
-	
-	$('.navbar-nav').onePageNav({
-		filter: ':not(.external)'
-	});
-
-
-	/* ========================================================== */
-	/*   SmoothScroll                                             */
-	/* ========================================================== */
-	
-	$(".navbar-nav li a, a.scrool").on('click', function(e) {
-		
-		var full_url = this.href;
-		var parts = full_url.split("#");
-		var trgt = parts[1];
-		var target_offset = $("#"+trgt).offset();
-		var target_top = target_offset.top;
-		
-		$('html,body').animate({scrollTop:target_top -70}, 1000);
-			return false;
-		
-	});	
-	
-
-	/* ========================================================== */
-	/*   Contact                                                  */
-	/* ========================================================== */
-	$('#contact-form').each( function(){
-		var form = $(this);
-		//form.validate();
-		form.submit(function(e) {
-			if (!e.isDefaultPrevented()) {
-				jQuery.post(this.action,{
-					'names':$('input[name="contact_names"]').val(),
-					'subject':$('input[name="contact_subject"]').val(),
-					'email':$('input[name="contact_email"]').val(),
-					'phone':$('input[name="contact_phone"]').val(),
-					'message':$('textarea[name="contact_message"]').val(),
-				},function(data){
-					form.fadeOut('fast', function() {
-						$(this).siblings('p').show();
-					});
-				});
-				e.preventDefault();
-			}
-		});
-	})
-});
-
-	/* ========================================================== */
-	/*   Popup-Gallery                                            */
-	/* ========================================================== */
-	$('.popup-gallery').find('a.popup1').magnificPopup({
-		type: 'image',
-		gallery: {
-		  enabled:true
-		}
-	}); 
-	
-	$('.popup-gallery').find('a.popup2').magnificPopup({
-		type: 'image',
-		gallery: {
-		  enabled:true
-		}
-	}); 
- 
-	$('.popup-gallery').find('a.popup3').magnificPopup({
-		type: 'image',
-		gallery: {
-		  enabled:true
-		}
-	}); 
- 
-	$('.popup-gallery').find('a.popup4').magnificPopup({
-		type: 'iframe',
-		gallery: {
-		  enabled:false
-		}
-	});  
- 
+/* Dark & Light Mode */
+function setColorScheme(scheme) {
+  let root = document.getElementsByTagName('html')[0];
+  switch (scheme) {
+    case 'dark':
+      // console.log(‘dark’);
+      root.classList.add('dark-mode');
+      document.getElementById('header-logo').src = '../assets/img/logo-dark.svg';
+      document.getElementById('footer-logo').src = '../assets/img/logo-dark.svg';
+      break;
+    case 'light':
+      // console.log(‘light’);
+      root.classList.remove('dark-mode');
+      document.getElementById('header-logo').src = '../assets/img/logo.svg';
+      document.getElementById('footer-logo').src = '../assets/img/logo.svg';
+      break;
+    default:
+      console.log('default');
+      break;
+  }
+}
+function getPreferredColorScheme() {
+  if (window.matchMedia) {
+    if (window.matchMedia('(prefers-color-scheme:dark)').matches) {
+      return 'dark';
+    } else {
+      return 'light';
+    }
+  }
+  return 'light';
+}
+function updateColorScheme() {
+  setColorScheme(getPreferredColorScheme());
+}
+if (window.matchMedia) {
+  var colorSchemeQuery = window.matchMedia('(prefers-color-scheme:dark)');
+  colorSchemeQuery.addEventListener('change', updateColorScheme);
+}
+updateColorScheme();
